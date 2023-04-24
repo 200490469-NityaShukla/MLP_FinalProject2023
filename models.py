6698a61625ad191a68effabe25abf9b14bf3862c
200490469-NityaShukla <106578145+200490469-NityaShukla@users.noreply.github.com>
2023-04-23 22:54:48 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
+# All rights are reserved.
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
